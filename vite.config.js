fc6cc74c6d18348e8059cb4c28e8e1d3ef7edbf6
Vishwas Web <vishwaslandge2004@gmail.com>
2024-12-11 13:33:00 +0530

    port added

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://robomartbackend.onrender.com', // Backend server
+        target: 'https://robomartbackend.onrender.com/3000', // Backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' prefix before forwarding
       },
